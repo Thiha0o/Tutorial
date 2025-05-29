efedfad3b44cae6e0cf3ce9d8e0c5da041655d1c
Thiha0o <thihaoo311@gmail.com>
2025-05-29 15:01:38 +0630

    Initialize React Lesson

---
+++ React/react-lessons/src/components/Example3.js
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+
+export default class Person extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Mg Kyaw Min",
+      date: new Date(),
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>State in Class Components</h1>
+        <div>{this.state.name}</div>
+        <div>{this.state.date.toString()}</div>
+      </div>
+    );
+  }
+}
