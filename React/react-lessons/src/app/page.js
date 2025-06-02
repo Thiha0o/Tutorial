aacedd7e780cf3c875f40e8a9ffc95ba77562ab3
Thiha0o <thihaoo311@gmail.com>
2025-06-02 14:59:02 +0630

    React lesson5-10

--- React/react-lessons/src/app/page.js
+++ React/react-lessons/src/app/page.js
@@ -8,6 +8,12 @@ import Exercise1 from "../exercises/Exercise1";
 import Exercise2 from "../exercises/Exercise2";
 import Exercise3 from "../exercises/Exercise3";
 import Exercise4 from "../exercises/Exercise4";
+import Example5 from "../components/Example5";
+import Example6 from "../components/Example6";
+import Example7 from "../components/Example7";
+import Example8 from "../components/Example8";
+import Example9 from "../components/Example9";
+import Example10 from "../components/Example10";
 
 export default function Home(){
   return (
@@ -16,10 +22,16 @@ export default function Home(){
       <Example2 />
       <Example3 />
       <Example4 /> */}
-      <Exercise1 />
+      {/* <Exercise1 />
       <Exercise2 />
       <Exercise3 />
-      <Exercise4 />
+      <Exercise4 /> */}
+      {/* <Example5 />
+      <Example6 />
+      <Example7 />
+      <Example8 /> */}
+      {/* <Example9 /> */}
+      <Example10 />
     </div>
   );
   } 
